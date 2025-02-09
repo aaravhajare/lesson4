2243a97487493fe0d411f854c4f70cb3b7687fca
aaravhajare <hajareaarav66@gmail.com>
2025-02-09 10:32:05 +0530

    percentage

---
+++ Percentage calculation.py
@@ -0,0 +1,13 @@
+
+maths = int(input("enter Math marks"))
+english = int(input("enter english marks"))
+hindi = int(input("enter Hindi marks"))
+ict = int(input("enter ICT marks"))
+sst = int(input("enter SST marks"))
+
+sum = maths + hindi + english + ict + sst
+
+percentage = (sum/500)*100
+
+print(end ="your percentage is :")
+print(percentage)
