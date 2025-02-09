8a90c5ea18e366915f46d74af604be9c20634946
aaravhajare <hajareaarav66@gmail.com>
2025-02-09 10:41:09 +0530

    homework

---
+++ homework.py
@@ -0,0 +1,10 @@
+
+num = 10
+sqr = (num)
+
+print (f" The number is  {num} and the square root is {sqr}")
+
+num = int(input("enter number"))
+sqr = num**0.5
+
+print(f"square root of {num} is {sqr} ")
